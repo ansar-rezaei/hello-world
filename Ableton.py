d41ac47c0311bb1b06a99599d961f8a7c7ccedfc
Ansar Rezazei <ansarrezazei@MyMac.fritz.box>
2022-07-24 21:41:17 +0200

    first parisa commit

--- Ableton.py
+++ Ableton.py
@@ -1,3 +1,7 @@
 print ("Hello From Iran")
 print ("2nd Commit")
 print ("We now add a branch to our repo, that it's named AB-1")
+print ("Hello From Iran")
+print ("2nd Commit")
+print ("We now add a branch to our repo, that it's named AB-1")print ("Hello From Iran")
+print ("2nd Commit")
